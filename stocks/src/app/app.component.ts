7c5c5aebc26c536537036a066442d844de21c7ad
Dan H <spag1024@hotmail.com>
2019-08-18 08:34:38 -0500

    adding stock service to app component

--- stocks/src/app/app.component.ts
+++ stocks/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StocksService, StockInterface } from 'src/app/services/stocks.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stocks';
+  stocks: Array<StockInterface>;
+
+  constructor(service: StocksService) {
+    service.load(['AAPL']).subscribe(stocks => {
+      this.stocks = stocks;
+    })
+  }
 }
